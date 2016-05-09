c96da9ae3ece65a99625fbbf6e5066579623ed5e
Purit Hemwadee <Purithem@purits-macbook-air.local>
2016-05-09 18:01:34 +0700

    Initial Commit

---
+++ super cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  super cool
+//
+//  Created by Purit Hemwadee on 5/9/16.
+//  Copyright © 2016 Purit Hemwadee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
